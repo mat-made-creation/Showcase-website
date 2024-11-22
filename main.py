421121137701126df9b2308d46fa5120b91542f9
Matthew <matthewturner@hotmail.co.nz>
2024-11-22 23:03:47 +0900

    Render the app titles in columns

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
@@ -11,3 +12,19 @@ with col2:
     st.title("Matthew")
     content = """Hi, I am Matthew. I am learning to program in Python"""
     st.write(content)
+
+content2 = """Here are some examples of apps that I have created in Python"""
+st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pd.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+
+with col4:
+    for index, row in df[11:].iterrows():
+        st.header(row["title"])
